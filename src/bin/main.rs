c6d74b5d19ff15083ae528ee24d6a1323718551f
Alexandre Shinebourne <alexandre@shinebourne.com>
2021-10-26 17:33:28 +0100

    working p2p abstraction layer without threadpool and shared known hosts access

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,32 +9,16 @@ use butter::line_codec::LineCodec;
 use butter::server::Server;
 // use crate::client::Client;
 use butter::threadpool::ThreadPool;
+use butter::peer_to_peer::PeerToPeer;
 
 // There are two types of sockets: Active and passive sockets. Active sockets are the ones which
 // have a peer connected at the other end and data can be sent and received at this socket. Passive
 // socket can just listen to connection requests - it can never talk to clients, send/receive data.
 
-lazy_static! {
-    static ref KNOWN_HOSTS: Mutex<Vec<IpAddr>> = Mutex::new(Vec::new());
-}
-
 /// Given a TcpStream:
 /// - Deserialize the message
 /// - Serialize and write the echo message to the stream
-fn handle_client(stream: TcpStream) {
-    // let mut data = [0 as u8; 50]; // using 50 byte buffer
-    // while match stream.read(&mut data) {
-    //     Ok(size) => {
-    //         // echo everything!
-    //         stream.write(&data[0..size]).unwrap();
-    //         true
-    //     }
-    //     Err(_) => {
-    //         println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
-    //         stream.shutdown(Shutdown::Both).unwrap();
-    //         false
-    //     }
-    // } {}
+fn server_behaviour(stream: TcpStream) {
     let mut codec = LineCodec::new(stream).unwrap();
 
     // Read & reverse the received message
@@ -48,90 +32,7 @@ fn handle_client(stream: TcpStream) {
     codec.send_message(&message).unwrap();
 }
 
-fn server_functionality() -> () {
-    let server: Server = Server::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8376);
-    let pool = ThreadPool::new(4);
-    for stream in server.listener.incoming() {
-        let stream = stream.unwrap();
-        pool.execute(|| {
-            let peer_address = stream.peer_addr().unwrap().ip();
-            println!("\tNew connection from: {}", peer_address);
-            handle_client(stream);
-            if !KNOWN_HOSTS.lock().unwrap().contains(&peer_address) {
-                KNOWN_HOSTS.lock().unwrap().push(peer_address);
-            }
-        });
-    }
-}
-
-
-// obviously can't work given that a new connection can't be formed
-fn client_functionality(request: &str) {
-    for i in KNOWN_HOSTS.lock().unwrap().iter() {
-        let address = i.to_string()+":8376";
-        let stream = TcpStream::connect(address).unwrap();
-        let mut codec = LineCodec::new(stream).unwrap();
-        codec.send_message(&request).unwrap();
-        println!("{}", codec.read_message().unwrap());
-    }
-    // Establish a TCP connection
-    // let stream = TcpStream::connect("127.0.0.1:8376").unwrap();
-
-    // Codec is our interface for reading/writing messages.
-    // No need to handle reading/writing directly
-    // let mut codec = LineCodec::new(stream).unwrap();
-
-    // Serializing & Sending is now just one line
-    // codec.send_message(&request).unwrap();
-
-    // And same with receiving the response!
-    // println!("{}", codec.read_message().unwrap());
-    // Ok(())
-
-    // TODO: Look at using the stream.shutdown() method...
-
-    // let client: Client = Client::new(0,0,0,0,0);
-    // match TcpStream::connect("127.0.0.1:8376") {
-    //     Ok(mut stream) => {
-    //         println!("Successfully connected to server.");
-    //
-    //         // let msg = b"Hello!";
-    //
-    //         stream.write(request.as_bytes()).unwrap();
-    //         println!("Sent request, awaiting reply...");
-    //
-    //         let mut data = [0 as u8; 6]; // using 6 byte buffer
-    //         match stream.read_exact(&mut data) {
-    //             Ok(_) => {
-    //                 if &data == request.as_bytes() {
-    //                     println!("Reply is ok!");
-    //                 } else {
-    //                     let text = from_utf8(&data).unwrap();
-    //                     println!("Unexpected reply: {}", text);
-    //                 }
-    //             }
-    //             Err(e) => {
-    //                 println!("Failed to receive data: {}", e);
-    //             }
-    //         }
-    //     }
-    //     Err(e) => {
-    //         println!("Failed to connect: {}", e);
-    //     }
-    // }
-}
-
-fn main() {
-    let entrypoint = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-
-    KNOWN_HOSTS.lock().unwrap().push(entrypoint);
-
-    thread::spawn(|| {
-        server_functionality();
-    });
-
-    thread::sleep(Duration::from_secs(2));
-
+fn client_behaviour() {
     loop {
         println!("Send a message:");
 
@@ -142,10 +43,23 @@ fn main() {
             .ok()
             .expect("Couldn't read line");
 
-        client_functionality(&input);
-
+        // for i in KNOWN_HOSTS.lock().unwrap().iter() {
+        //     let address = i.to_string() + ":8376";
+        //     let stream = TcpStream::connect(address).unwrap();
+        //     let mut codec = LineCodec::new(stream).unwrap();
+        //     codec.send_message(&request).unwrap();
+        //     println!("{}", codec.read_message().unwrap());
+        // }
+        let stream = TcpStream::connect("127.0.0.1:8376").unwrap();
+        let mut codec = LineCodec::new(stream).unwrap();
+        codec.send_message(&input).unwrap();
+        println!("{}", codec.read_message().unwrap());
     }
-    println!("Terminated.");
+}
+
+fn main() {
+    let p2p: PeerToPeer = PeerToPeer::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+                                          8376, server_behaviour, client_behaviour);
 }
 
 
--- src/peer_to_peer.rs
+++ src/peer_to_peer.rs
@@ -1,25 +1,61 @@
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv4Addr, TcpStream};
+use std::thread;
+use std::time::Duration;
 use crate::server::Server;
+use crate::threadpool::ThreadPool;
 
+use lazy_static::lazy_static;
+// use crate::line_codec::LineCodec;
 
-struct PeerToPeer {
-    server: Server,
-    known_hosts: Vec<IpAddr>,
+use std::sync::Mutex;
 
+lazy_static! {
+    static ref KNOWN_HOSTS: Mutex<Vec<IpAddr>> = Mutex::new(Vec::new());
+}
+
+pub struct PeerToPeer {
+    // server: Server,
+    // known_hosts: KNOWN_HOSTS,
     ip_address: IpAddr,
     port: u16,
 }
 
 impl PeerToPeer {
-    fn new(ip_address: IpAddr, port: u16, server_functionality: &dyn Fn() -> (),
-           client_functionality: &dyn Fn() -> ()) -> PeerToPeer {
+    pub fn new(ip_address: IpAddr, port: u16, server_behaviour: fn(TcpStream) -> (),
+               client_behaviour: fn() -> ()) -> PeerToPeer {
 
-        let server: Server = Server::new(ip_address, port);
         // known_hosts - add itself?
+        let entrypoint = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+        KNOWN_HOSTS.lock().unwrap().push(entrypoint);
+
+        thread::spawn(move || {
+            let server: Server = Server::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8376);
+            // let pool = ThreadPool::new(4);
+            for stream in server.listener.incoming() {
+                let stream = stream.unwrap();
+                // pool.execute(|| {
+                //     let peer_address = stream.peer_addr().unwrap().ip();
+                //     println!("\tNew connection from: {}", peer_address);
+                //     server_behaviour(stream);
+                //     if !KNOWN_HOSTS.lock().unwrap().contains(&peer_address) {
+                //         KNOWN_HOSTS.lock().unwrap().push(peer_address);
+                //     }
+                // });
+                let peer_address = stream.peer_addr().unwrap().ip();
+                println!("\tNew connection from: {}", peer_address);
+                server_behaviour(stream);
+                if !KNOWN_HOSTS.lock().unwrap().contains(&peer_address) {
+                    KNOWN_HOSTS.lock().unwrap().push(peer_address);
+                }
+            }
+        });
+
+        // Allow the server to startup - what about just putting the server on the main thread?
+        thread::sleep(Duration::from_secs(2));
+
+        client_behaviour();
 
         PeerToPeer {
-            server,
-            known_hosts,
             ip_address,
             port
         }
